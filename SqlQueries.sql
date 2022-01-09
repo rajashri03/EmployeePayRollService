78f9d50580084b1f1b854185af9c2176f15bcbd7
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-09 16:12:36 +0530

    [Rajashri]Add:retrive data for particular condition from table

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -1,8 +1,12 @@
-/*Create database*/
+/*UC1-Create database*/
 Create database PayrollService;
-/*Create Table*/
+/*UC2-Create Table*/
 Create table Employee_PayRoll(id int identity(1,1)primary key,EmployeeName varchar(200),Salary varchar(200),startdate DATE);
-/*Insert Data*/
+/*UC3-Insert Data*/
 Insert into Employee_PayRoll(EmployeeName,Salary,startdate)values('Rajashri','35000','2021/2/3');
-/*Retrive Data*/
+/*UC4-Retrive Data*/
 Select * from Employee_PayRoll;
+/*UC5-Retrive Data from particular condition*/
+/*select Salary of particular employee*/
+select Salary from Employee_PayRoll where EmployeeName='Rajashri';
+select * from Employee_PayRoll WHERE startdate BETWEEN CAST('2021-01-01' AS DATE)and CAST('2021-02-03' AS DATE);
