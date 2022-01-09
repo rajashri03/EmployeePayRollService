7a6b53771c4b90b18afe606eb05ced4cbbaa6741
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-09 15:56:09 +0530

    [Rajashri]Add:retrive data in employeepayroll table

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -1,6 +1,8 @@
 /*Create database*/
 Create database PayrollService;
 /*Create Table*/
-Create table Employee_PayRoll(id int identity(1,1)primary key,EmployeeName varchar(200),Salary varchar,startdate DATE);
+Create table Employee_PayRoll(id int identity(1,1)primary key,EmployeeName varchar(200),Salary varchar(200),startdate DATE);
 /*Insert Data*/
-Insert into Employee_PayRoll(EmployeeName,Salary,startdate)values('Rajashri',25000,'2021/2/3');
+Insert into Employee_PayRoll(EmployeeName,Salary,startdate)values('Rajashri','35000','2021/2/3');
+/*Retrive Data*/
+Select * from Employee_PayRoll;
