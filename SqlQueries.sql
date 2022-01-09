383363d62edcb491101e7e8cae3fe050b3b27aa4
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-09 15:46:17 +0530

    [Rajashri]Add:Insert data in employeepayroll table

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -2,3 +2,5 @@
 Create database PayrollService;
 /*Create Table*/
 Create table Employee_PayRoll(id int identity(1,1)primary key,EmployeeName varchar(200),Salary varchar,startdate DATE);
+/*Insert Data*/
+Insert into Employee_PayRoll(EmployeeName,Salary,startdate)values('Rajashri',25000,'2021/2/3');
