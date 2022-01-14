6303e40937299aaa5d24e4dbb664d92812212b19
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-14 17:11:14 +0530

    [Rajashr]Add:Create Database->Ado.net

---
+++ PayRollService/PayRollService/EmployeeData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRollService
+{
+    public class EmployeeData
+    {
+        /// <summary>
+        /// Create Database-Payroll Service
+        /// </summary>
+        public static void Create_Database()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=AD-PC\SQLEXPRESS; Initial Catalog =master; Integrated Security = True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create database PayrollService;;", con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("PayrollService; Database created successfully!");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+    }
+}
---
+++ PayRollService/PayRollService/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PayRollService;
+
+Console.WriteLine("------------PayRoll Service--------");
+EmployeeData.Create_Database();
